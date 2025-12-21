96a2237ef0839d93f9a7c59663625baaf0ec76e8
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-12-21 22:14:11 +0530

    Subjects data passed to db comp to email

--- lib/views/main/teacher/pages/subject_selection.dart
+++ lib/views/main/teacher/pages/subject_selection.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ucbs_attendance_app/colors/colors.dart';
 import 'package:ucbs_attendance_app/methods/supabase/get_subject_name.dart';
+import 'package:ucbs_attendance_app/provider/user_session.dart';
+import 'package:ucbs_attendance_app/views/main/teacher/pages/teacher_mainpage.dart';
 
 class SubjectSelection extends StatefulWidget {
   const SubjectSelection({super.key});
@@ -61,7 +66,7 @@ class _SubjectSelectionState extends State<SubjectSelection> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
             child: Container(
-              height: 780,
+              height: 700,
               width: double.infinity,
               decoration: BoxDecoration(
                 color: AppColors.bgLightDark,
@@ -139,6 +144,76 @@ class _SubjectSelectionState extends State<SubjectSelection> {
                     ),
                   );
                 },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: GestureDetector(
+              onTap: () async {
+                final String? userEmail = context.read<UserSession>().email;
+                if (userEmail == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Fill ALL INFORMATION WITHOUT CLOSING APP"),
+                    ),
+                  );
+                  return;
+                }
+
+                final List<String> selectedSubjects = selectedIndexes
+                    .map((i) => subjects[i]['subjectsName'] as String)
+                    .toList();
+
+                if (selectedSubjects.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Select at least one subject"),
+                    ),
+                  );
+                  return;
+                }
+
+                final supabase = Supabase.instance.client;
+
+                await supabase
+                    .from('teachers')
+                    .update({'subject': selectedSubjects})
+                    .eq('email', userEmail);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    backgroundColor: AppColors.success,
+                    content: Text("Subjects saved successfully"),
+                  ),
+                );
+                final prefs = await SharedPreferences.getInstance();
+                prefs.setBool('isLogged', true);
+                Future.delayed(Duration(seconds: 3), () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => TeacherMainpage()),
+                  );
+                });
+              },
+
+              child: Container(
+                height: 70,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.accentBlue,
+                ),
+                child: Center(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: AppColors.textPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
