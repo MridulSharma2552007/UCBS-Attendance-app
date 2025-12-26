5409a5932548a1880c951e4060e0f95f3834df8f
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-12-26 22:10:47 +0530

    ignore api keys

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:ucbs_attendance_app/apis/apikey.dart';
+;
 import 'package:ucbs_attendance_app/firebase_options.dart';
 import 'package:ucbs_attendance_app/provider/Data/user_session.dart';
 import 'package:ucbs_attendance_app/views/login/Gates/app_gate.dart';
