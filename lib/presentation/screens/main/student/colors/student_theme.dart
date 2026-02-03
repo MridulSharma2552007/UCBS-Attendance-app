3d135dfdbe863fc5c0cf9bb1d60cb3e50a552457
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-02-03 21:19:06 +0530

    add StudentTheme class with color constants for the app's UI

---
+++ lib/presentation/screens/main/student/colors/student_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StudentTheme {
+  static const Color backgroundColor = Color(0xffE8ECEF);
+  static const Color backgrounddarkColor = Color(0xff0D0D0D);
+  static const Color cardcolor = Color(0xff1A1A1A);
+  static const Color accentcoral = Color(0xffFF7171);
+  static const Color primarypink = Color(0xfff472b6);
+}
