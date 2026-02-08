f53687acbc3d808b1a48693d349500b484f1d306
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-02-08 18:17:55 +0530

    fixed init call

--- lib/presentation/screens/main/student/pages/student_main_screen.dart
+++ lib/presentation/screens/main/student/pages/student_main_screen.dart
@@ -40,6 +40,7 @@ class _StudentMainScreenState extends State<StudentMainScreen> with WidgetsBindi
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
+      _loadLiveClasses();
       _loadAttendance();
     }
   }
